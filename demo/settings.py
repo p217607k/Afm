e77aed0686a8bd6ccc1f2b81adb5b8e1d470dcc7
Pratap Kumar <pk949542@gmail.com>
2022-10-20 11:55:29 +0530

    first commit

--- demo/settings.py
+++ demo/settings.py
@@ -100,22 +100,22 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'genorionpro',
-#         'PASSWORD': 'genorionpro',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'genorionpro',
+        'PASSWORD': 'genorionpro',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
